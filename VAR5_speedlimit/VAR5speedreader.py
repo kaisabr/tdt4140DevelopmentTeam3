e5fbf3490b0bf3a32893dffc58252f81c533ce0e
arnagl <arna.glavovic@gmail.com>
2016-02-25 11:54:21 +0100

    Prøver å importere speedreader-klassen til test. Failer.

--- VAR5_speedlimit/VAR5speedreader.py
+++ VAR5_speedlimit/VAR5speedreader.py
@@ -1,10 +1,19 @@
 
 
+currentSpeedLimit = 80
+speed = [30, 45, 69, 88, 30, 72, 1000]
+constant = 1.1
+
 class speedLimit():
 
-    speedLimit = 80
-    speed = [30, 45, 69, 88, 30, 72, 100]
-    constant = 1.1
+    def drivingToFast(self, mySpeed, speedLimit):
 
-    def drivingToFast(self, speed, speedlimit):
-        if(speed > (speedLimit*self.constant)):
+        for x in range(len(mySpeed)):
+            if(mySpeed[x] <= (speedLimit*constant)):
+                print "OK"
+            else:
+                print "Driving to fast"
+
+
+testSpeed = speedLimit()
+testSpeed.drivingToFast(speed, currentSpeedLimit)
--- VAR5_speedlimit/VAR5test.py
+++ VAR5_speedlimit/VAR5test.py
@@ -1,18 +1,25 @@
 import unittest
+import VAR5speedreader as sp
 from VAR5speedreader import *
 
-constant = 1.1
 
 class MyTestCase(unittest.TestCase):
 
     # Testing if the code is giving you a message if you're driving to fast (over the speed limit)
-    def testToFast(self, speed, speedlimit):
-        self.assertTrue(speed < speedlimit*constant, "Driving to fast")
+    def testToFast(self, sp.speed, sp.currentSpeedLimit):
+        for x in range(len(speed)):
+            self.assertTrue(speed[x] < currentSpeedLimit*constant, "Driving to fast")
 
     #Testing if the code is giving you a message if you don't drive faster than the speed limit.
-    def testPerfectSpeed(self, speed, speedlimit):
-        self.assertTrue(speed > speedlimit*constant, "OK")
+    def testPerfectSpeed(self, sp.speed, sp.currentSpeedLimit):
+        for x in range(len(speed)):
+            self.assertTrue(speed[x] > currentSpeedLimit*constant, "OK")
 
+
+
+#test = MyTestCase()
+#test.testToFast(SP.speed, SP.currentSpeedLimit)
+#test.testPerfectSpeed(SP.speed, SP.currentSpeedLimit)
 
 
 if __name__ == '__main__':
