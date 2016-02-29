0998b8329c2b8ff94cd7a3915360b6bf50f18061
Ashild <ashild.bekken@gmail.com>
2016-02-29 11:43:47 +0100

    Oppdatert test for var3

---
+++ var3/main_var3.py
@@ -0,0 +1,34 @@
+# Skrevet av Magnus og Aashild
+
+import random
+import time
+
+
+def main():
+
+# Returnerer distanse til bilen foran
+def getDistance():
+    return random.randrange(5, 20)
+
+# Returnerer fart i meter per sekund
+def getSpeed():
+    return random.randrange(0,30)
+
+# Beregner optimal distanse
+def calculateOptimalDistance(speed):
+    return getSpeed()*3
+
+# Sjekker om bilen er paa
+def carIsOn():
+    return True
+
+# Sirenen varsler dersom avstanden til bilen foran er for liten
+def siren():
+    while carIsOn:
+        time.sleep(1)
+        if (calculateOptimalDistance(getSpeed()) > getDistance()):
+            MakeSiren = True
+            return MakeSiren
+        else:
+            MakeSiren = False
+            return MakeSiren
--- var3/test_var3.py
+++ var3/test_var3.py
@@ -1,34 +1,22 @@
+# Skrevet av Magnus og Aashild
+
+
 import unittest
-
-def main():
-
-
-def getDistance():
-
-def getSpeed():
-
-def calculateOptimalDistance(speed):
-
-
-def siren():
+import main_var3
 
 
 MakeSiren = True
 
-distance = 0
-speed = 0
 
 class MyTest(unittest.TestCase):
     def var3_test(self):
-        self.assertTrue(getDistance() > 0)
-        self.assertTrue(getSpeed() > 0)
-        self.assertTrue(calculateOptimalDistance(speed) > 0)
-        self.assertEqual(calculateOptimalDistance(30), 90)
-        if getDistance() < calculateOptimalDistance(speed):
+        self.assertTrue(main_var3.getDistance() > 0)
+        self.assertTrue(main_var3.getSpeed() > 0)
+        self.assertTrue(main_var3.calculateOptimalDistance(main_var3.getSpeed()) > 0)
+        self.assertEqual(main_var3.calculateOptimalDistance(30), 90)
+        if main_var3.getDistance() < main_var3.calculateOptimalDistance(main_var3.getSpeed()):
             self.assertTrue(MakeSiren)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-# time.sleep()
