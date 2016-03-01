a1865b654a36619766582351b7aee16d48ba6af2
Ashild <ashild.bekken@gmail.com>
2016-03-01 15:34:27 +0100

    Oppdatert test for var3

--- var3/main_var3.py
+++ var3/main_var3.py
@@ -4,31 +4,30 @@ import random
 import time
 
 
-def main():
 
 # Returnerer distanse til bilen foran
 def getDistance():
-    return random.randrange(5, 20)
+	return random.randrange(5, 20)
 
 # Returnerer fart i meter per sekund
 def getSpeed():
-    return random.randrange(0,30)
+	return random.randrange(0,30)
 
 # Beregner optimal distanse
 def calculateOptimalDistance(speed):
-    return getSpeed()*3
+	return getSpeed()*3
 
 # Sjekker om bilen er paa
 def carIsOn():
-    return True
+	return True
 
 # Sirenen varsler dersom avstanden til bilen foran er for liten
 def siren():
-    while carIsOn:
-        time.sleep(1)
-        if (calculateOptimalDistance(getSpeed()) > getDistance()):
-            MakeSiren = True
-            return MakeSiren
-        else:
-            MakeSiren = False
-            return MakeSiren
+	while carIsOn:
+		time.sleep(1)
+		if (calculateOptimalDistance(getSpeed()) > getDistance()):
+			MakeSiren = True
+			return MakeSiren
+		else:
+			MakeSiren = False
+			return MakeSiren
--- var3/test_var3.py
+++ var3/test_var3.py
@@ -9,11 +9,15 @@ MakeSiren = True
 
 
 class MyTest(unittest.TestCase):
-    def var3_test(self):
+    def test1(self):
         self.assertTrue(main_var3.getDistance() > 0)
+    def test2(self):
         self.assertTrue(main_var3.getSpeed() > 0)
+    def test3(self):
         self.assertTrue(main_var3.calculateOptimalDistance(main_var3.getSpeed()) > 0)
+    def test4(self):
         self.assertEqual(main_var3.calculateOptimalDistance(30), 90)
+    def test5(self):
         if main_var3.getDistance() < main_var3.calculateOptimalDistance(main_var3.getSpeed()):
             self.assertTrue(MakeSiren)
 
