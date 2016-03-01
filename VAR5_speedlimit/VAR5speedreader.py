dd0bbed2a7d4cee36c2df02cb5d7f560de7a1b7e
Mathilde Theisen <mathildetheisen@gmail.com>
2016-03-01 20:31:04 +0100

    Lagt til kode for å ta inn input for fart og fartsgrense, og printer de slik at jeg ser at det er rett.

--- VAR5_speedlimit/VAR5speedreader.py
+++ VAR5_speedlimit/VAR5speedreader.py
@@ -1,6 +1,14 @@
+from itertools import izip
+
 constant = 1.1
 
 class SpeedLimit():
+
+    with open("Speed.txt") as speedInput, open("SpeedLimit.txt") as speedLimitInput:
+        for lineInSpeedInput, lineInSpeedLimitInput in izip(speedInput, speedLimitInput):
+            x = lineInSpeedInput.strip()
+            y = lineInSpeedLimitInput.strip()
+            print "{0}\t{1}".format(x, y)
 
     def drivingOK(self, mySpeed, speedLimit):
          for x in range(len(mySpeed)):
