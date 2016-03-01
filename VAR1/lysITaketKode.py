f8e54bb97053197cce8126fb527f439bdfff2e57
Andrea Lind <andrefli@stud.ntnu.no>
2016-03-01 16:51:36 +0100

    Kode for usercase taklys

--- VAR1/lysITaketKode.py
+++ VAR1/lysITaketKode.py
@@ -1,8 +1,9 @@
-def taklys(l, d, f):
-    if l == 1 and d == 0 and f == 0:
+#metoden returnerer false dersom alt er OK (ingen notification til kjorer) ellers true
+def taklys(l, d, f): #tar inn en verdi for om lys er paa/av, dor aapen/lukket, bil fart/stille, alle verdier 0 eller 1
+    if l == 1 and d == 0 and f == 0: #eneste gangen det er ok at lyset faktisk er paa
         return False
-    elif l==1:
-        return True #sjekker om lyset er på
+    elif l==1: #om den gaar hit, vil enten dor vaere aapen og/eller bil ha fart
+        return True
     else:
-        return False
+        return False #lyset er av, skal ikke varsle om noe
 
--- VAR1/testVar1.py
+++ VAR1/testVar1.py
@@ -2,32 +2,37 @@ import unittest
 from lysITaketKode import taklys
 lys = [1,1,1,1,0,0,0,0] #Tester alle mulige kombinasjoner
 dor = [0,1,0,1,1,1,0,0]
-fart = [0,0,1,1,0,1,1,0]
+fart1 = open('farter.txt','r') #aapner fil med hastigheter til bilen
+fart=[] #oppretter tom liste for aa legge inn 0 eller 1 ettersom bilen staar stille eller ikke
 
-lys2 = [10110001]
-dor2 = [01011101]
-fart2 = [00110110]
+for line in fart1: #legger inn 1 naar bilen har fart, og 0 naar bilen staar stille
+
+    if line.strip() != str(0):
+        fart.append(1) #bil har fart
+    else:
+        fart.append(0) #bil har ikke fart
+print(fart)
 
 
-class TestUM(unittest.TestCase):
+class TestUM(unittest.TestCase):#gir ut true dersom den skal pipe, false om den ikke skal pipe
     def setUp(self):
         pass
     def test1(self):
-        self.assertEqual(taklys(lys[0],dor[0],fart[0]), False)
+        self.assertEqual(taklys(lys[0],dor[0],fart[0]), False) #lys paa, dor lukket, bil stille
     def test2(self):
-        self.assertEqual(taklys(lys[1],dor[1],fart[1]), True)
+        self.assertEqual(taklys(lys[1],dor[1],fart[1]), True) #lys paa, dor aapen, bil stille
     def test3(self):
-        self.assertEqual(taklys(lys[2],dor[2],fart[2]), True)
+        self.assertEqual(taklys(lys[2],dor[2],fart[2]), True) #lys paa, dor lukket, bil fart
     def test4(self):
-        self.assertEqual(taklys(lys[3],dor[3],fart[3]), True)
-    def test5(self):
-        self.assertEqual(taklys(lys[4],dor[4],fart[4]), False) 
+        self.assertEqual(taklys(lys[3],dor[3],fart[3]), True) #lys paa, dor aapen, bil fart
+    def test5(self): #lys av, dor aapen, bil stille
+        self.assertEqual(taklys(lys[4],dor[4],fart[4]), False) #lys av, dor aapen, bil stille
     def test6(self):
-        self.assertEqual(taklys(lys[5],dor[5],fart[5]), False)
+        self.assertEqual(taklys(lys[5],dor[5],fart[5]), False) #lys av, dor aapen, bil fart
     def test7(self):
-        self.assertEqual(taklys(lys[6],dor[6],fart[6]), False)
+        self.assertEqual(taklys(lys[6],dor[6],fart[6]), False) #lys av, dor lukket, bil fart
     def test8(self):
-        self.assertEqual(taklys(lys[7],dor[7],fart[7]), False)
+        self.assertEqual(taklys(lys[7],dor[7],fart[7]), False) #lys av, dor lukket, bil stille
 
 
 if __name__=='__main__':
