03f0bd98fc2eae02a44e9c772ddf744e7a05e6b2
Mathilde Theisen <mathildetheisen@gmail.com>
2016-03-01 21:39:00 +0100

    Prøvd å gjøre slik at koden blir rett. TypeError: drivingOK() takes exactly 3 arguments (2 given)....

--- VAR5_speedlimit/VAR5speedreader.py
+++ VAR5_speedlimit/VAR5speedreader.py
@@ -4,21 +4,31 @@ constant = 1.1
 
 class SpeedLimit():
 
-    with open("Speed.txt") as speedInput, open("SpeedLimit.txt") as speedLimitInput:
-        for lineInSpeedInput, lineInSpeedLimitInput in izip(speedInput, speedLimitInput):
-            x = lineInSpeedInput.strip()
-            y = lineInSpeedLimitInput.strip()
-            print "{0}\t{1}".format(x, y)
-
     def drivingOK(self, mySpeed, speedLimit):
-         for x in range(len(mySpeed)):
-             if mySpeed[x] <= (speedLimit*constant):
-                 return True
-             else:
-                 return False
+        if mySpeed <= (speedLimit*constant):
+            return True
+        else:
+            return False
+        # for x in range(len(mySpeed)):
+        #     if mySpeed[x] <= (speedLimit*constant):
+        #         return True
+        #     else:
+        #         return False
 
     def drivingToFast(self, myspeed, speedLimit):
         if (myspeed <= (speedLimit*constant)):
             return False
         else:
             return True
+
+
+    with open("Speed.txt") as speedInput, open("SpeedLimit.txt") as speedLimitInput:
+        for lineInSpeedInput, lineInSpeedLimitInput in izip(speedInput, speedLimitInput):
+            x = lineInSpeedInput.strip()
+            y = lineInSpeedLimitInput.strip()
+            print "{0}\t{1}".format(x, y)
+
+            if drivingOK(x, y):
+                print "OK"
+            if drivingToFast(x, y):
+                print "Driving to Fast"
--- VAR5_speedlimit/VAR5test.py
+++ VAR5_speedlimit/VAR5test.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import izip
 from VAR5speedreader import *
 
 
@@ -13,17 +14,19 @@ class MyTestCase(unittest.TestCase):
 
     # Testing if the code is giving you a message if you're driving to fast (over the speed limit)
     def testToFast(self):
-        self.assertTrue(SpeedLimit().drivingOK(speed_list_to_fast, currentSpeedLimit), False)
+        # self.assertTrue(SpeedLimit().drivingOK(speed_list_to_fast, currentSpeedLimit), False)
+        self.assertTrue(SpeedLimit().drivingOK(speed_to_fast, currentSpeedLimit), False)
 
-    #Testing if the code is giving you a message if you don't drive faster than the speed limit.
+    # Testing if the code is giving you a message if you don't drive faster than the speed limit.
     def testPerfectSpeed(self):
-        self.assertTrue(SpeedLimit().drivingOK(speed_list_perfect, currentSpeedLimit), True)
+        # self.assertTrue(SpeedLimit().drivingOK(speed_list_perfect, currentSpeedLimit), True)
+        self.assertTrue(SpeedLimit().drivingOK(speed_OK, currentSpeedLimit), True)
 
     def test_DrivingToFast(self):
-         self.assertFalse(SpeedLimit().drivingToFast(speed, currentSpeedLimit), "Driving to Fast")
+        self.assertFalse(SpeedLimit().drivingToFast(speed, currentSpeedLimit), "Driving to Fast")
 
     def test_OK(self):
-         self.assertFalse(SpeedLimit().drivingToFast(speed, currentSpeedLimit))
+        self.assertFalse(SpeedLimit().drivingToFast(speed, currentSpeedLimit))
 
     def test_DrivingToFast2(self):
          self.assertEquals(SpeedLimit().drivingToFast(speed_to_fast, currentSpeedLimit), True, "You are driving to fast")
