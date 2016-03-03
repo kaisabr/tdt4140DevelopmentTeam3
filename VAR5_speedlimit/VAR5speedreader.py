4dc4c1eb8fb956f8d997ccece52a0511b8c0c704
arnagl <arna.glavovic@gmail.com>
2016-03-03 10:33:29 +0100

    Koden er OK. Kommentert kode.

--- VAR5_speedlimit/VAR5speedreader.py
+++ VAR5_speedlimit/VAR5speedreader.py
@@ -4,27 +4,32 @@ constant = 1.1
 
 class SpeedLimit():
 
+    # Open file
     def openFile(self, mySpeed, speedLimit):
         with open(mySpeed, 'r') as speedInput, open(speedLimit, 'r') as speedLimitInput:
-
+            # Iterating over the two files, one for speed and one for speed limit
             for speedLine, speedLimitLine in izip(speedInput, speedLimitInput):
+                # converting to int
                 speedLine, speedLimitLine = int(speedLine.split("\t")[0]), int(speedLimitLine.split("\t")[0])
 
+                # Check if there is no empty line
                 if (speedLine == '' or speedLimitLine == ''):
                     return None
                 else:
                     x = speedLine
                     y = speedLimitLine
 
+                    # checking speed
                     if x <= (y*constant):
                         return True
                     else:
                         self.sound()
                         return False
-
+        # closing files
         speedInput.close()
         speedLimitInput.close()
 
+    # warning sound
     def sound(self):
         return "Pip"
 
