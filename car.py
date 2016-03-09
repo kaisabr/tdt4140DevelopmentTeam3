718fe8a3aa5261102f5ef92ddb149e7597eaa408
Mathilde Theisen <mathildetheisen@gmail.com>
2016-03-09 11:37:09 +0100

    Lagt inn kommentarer på car.py

--- car.py
+++ car.py
@@ -1,6 +1,6 @@
 def getSpeed(speeds):
-    for line in speeds:
-        return speeds.readline()
+    for line in speeds: # For hver linje i inputfilen (som inneholder en fart per linje)
+        return speeds.readline() # returnerer en int med farten (som endrer seg kontinuerlig).
 
 def isInteriorLightOn():
     pass
