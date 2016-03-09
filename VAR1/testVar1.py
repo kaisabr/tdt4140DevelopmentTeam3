18972f97c35c99b3d698e4fe5697d99ec8eb56ed
kaisarokne <kaisa.rokne@gmail.com>
2016-03-09 12:29:40 +0100

    Endret navn på metodene

--- VAR1/testVar1.py
+++ VAR1/testVar1.py
@@ -1,7 +1,7 @@
 import unittest
-import lysITaketKode
+import interiorLightAnalyzer
 
-TL = lysITaketKode.interior_light()
+TL = interiorLightAnalyzer.interiorLightCheck()
 
 lights = [1,1,1,1,0,0,0,0] #Test all possible combinations
 door = [0,1,0,1,1,1,0,0]
@@ -13,21 +13,21 @@ class TestUM(unittest.TestCase):#give true if system should 'pip', if not, false
     def setUp(self):
         pass
     def test1(self):
-        self.assertEqual(TL.interior_light_check(lights[0],door[0],speed[0]), False) #light on, door closed, 0 speed
+        self.assertEqual(TL.interiorLightCheck(lights[0],door[0],speed[0]), False) #light on, door closed, 0 speed
     def test2(self):
-        self.assertEqual(TL.interior_light_check(lights[1],door[1],speed[1]), True) #light on, door open, 0 speed
+        self.assertEqual(TL.interiorLightCheck(lights[1],door[1],speed[1]), True) #light on, door open, 0 speed
     def test3(self):
-        self.assertEqual(TL.interior_light_check(lights[2],door[2],speed[2]), True) #light on, door closed, car with speed
+        self.assertEqual(TL.interiorLightCheck(lights[2],door[2],speed[2]), True) #light on, door closed, car with speed
     def test4(self):
-        self.assertEqual(TL.interior_light_check(lights[3],door[3],speed[3]), True) #light on, door open, car with speed
+        self.assertEqual(TL.interiorLightCheck(lights[3],door[3],speed[3]), True) #light on, door open, car with speed
     def test5(self): #lys av, dor aapen, bil stille
-        self.assertEqual(TL.interior_light_check(lights[4],door[4],speed[4]), False) #light off, door open, 0 speed
+        self.assertEqual(TL.interiorLightCheck(lights[4],door[4],speed[4]), False) #light off, door open, 0 speed
     def test6(self):
-        self.assertEqual(TL.interior_light_check(lights[5],door[5],speed[5]), False) #light off, door open, car with speed
+        self.assertEqual(TL.interiorLightCheck(lights[5],door[5],speed[5]), False) #light off, door open, car with speed
     def test7(self):
-        self.assertEqual(TL.interior_light_check(lights[6],door[6],speed[6]), False) #light off, door closed, car with speed
+        self.assertEqual(TL.interiorLightCheck(lights[6],door[6],speed[6]), False) #light off, door closed, car with speed
     def test8(self):
-        self.assertEqual(TL.interior_light_check(lights[7],door[7],speed[7]), False) #light off, door closed, 0 speed
+        self.assertEqual(TL.interiorLightCheck(lights[7],door[7],speed[7]), False) #light off, door closed, 0 speed
 
 
 if __name__=='__main__':
