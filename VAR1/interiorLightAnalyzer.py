0962a3a22fa0a3835ba30bfd19bf0d2e0cfa049e
Andrea Lind <andrefli@stud.ntnu.no>
2016-03-09 13:08:10 +0100

    InteriorLight oppdatering

--- VAR1/interiorLightAnalyzer.py
+++ VAR1/interiorLightAnalyzer.py
@@ -1,21 +1,29 @@
 #class for interior light
+import carInput
 
 class interiorLightAnalyzer:
     #Method returns the speeds in 0s and 1s
+    def convertSpeed(self, speed):
+        if speed != 0:
+            return 1
+        else:
+            return 0
 
         
     #the method returns flase if everything is OK (no notifications), if not, it returns false
-    def interiorLightCheck(self,l, d, s): #take in value for light, door and speed (1 or 0)
-    #siren =  Siren.siren(boolean), i følge UML-diagrammet har sirene-klassen en boolsk sirene-metode
-        if l == 1 and d == 0 and s == 0: #the only time it is ok for the light to be on
-            return False
-            #siren = Siren.siren(False)
-        elif l==1: #light is on, but dor is open or/and the car has a speed
-            return True
-            #siren = Siren.siren(True)
-        else:
-            return False #the light is off, no notification necessary
-            #siren = Siren.siren(False)
+    def interiorLightCheck(self,l, d): #take in value for light, door and speed (1 or 0)
+        speed = carInput.getSpeed()
+        while speed:
+            s = interiorLightAnalyzer.convertSpeed(speed)
+            if l == 1 and d == 0 and s == 0: #the only time it is ok for the light to be on
+                return False
+                #siren = Siren.siren(False)
+            elif l==1: #light is on, but dor is open or/and the car has a speed
+                return True
+                #siren = Siren.siren(True)
+            else:
+                return False #the light is off, no notification necessary
+                #siren = Siren.siren(False)
             
     
 
