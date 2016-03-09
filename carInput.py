e01a505b8f6004b8e036bf69a903c29c2bb66d5a
Ashild <ashild.bekken@gmail.com>
2016-03-09 11:46:10 +0100

    Lag til klasser / metoder i siren og carInput

--- carInput.py
+++ carInput.py
@@ -1,12 +1,12 @@
-class Car():
+class carInput():
 
-    def getSpeed(speeds):
+    def getSpeed(self, speeds):
         for line in speeds: # For hver linje i inputfilen (som inneholder en fart per linje)
             return speeds.readline() # returnerer en int med farten (som endrer seg kontinuerlig).
 
-    def isInteriorLightOn():
+    def isInteriorLightOn(self):
         pass
 
-    def distanceToCar():
+    def distanceToCar(self):
         pass
 
