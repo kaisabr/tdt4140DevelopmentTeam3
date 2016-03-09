0a0f72b7b19298a98d256570eb1677e8aa3180fb
Andrea Lind <andrefli@stud.ntnu.no>
2016-03-09 13:04:03 +0100

    CarInput-klasse med metoder 1.0

--- VAR1/interiorLightAnalyzer.py
+++ VAR1/interiorLightAnalyzer.py
@@ -2,15 +2,7 @@
 
 class interiorLightAnalyzer:
     #Method returns the speeds in 0s and 1s
-    def get_speeds(self,speed_file):
-        speed1 = open(speed_file,'r')
-        speed=[]
-        for line in speed1: #puts 1 in array when car has speed, 0 if not
-            if line.strip() != str(0):
-                speed.append(1) #car has speed
-            else:
-                speed.append(0) #car is standing still
-        return speed
+
         
     #the method returns flase if everything is OK (no notifications), if not, it returns false
     def interiorLightCheck(self,l, d, s): #take in value for light, door and speed (1 or 0)
--- carInput.py
+++ carInput.py
@@ -1,8 +1,15 @@
 class carInput():
 
-    def currentSpeed(self, speeds):
-        for line in speeds: # For hver linje i inputfilen (som inneholder en fart per linje)
-            return speeds.readline() # returnerer en int med farten (som endrer seg kontinuerlig).
+    def currentSpeed(self):
+        speeds = open('speedFile.txt','r')
+        for line in speeds:
+            if line.strip() == "\n":
+                pass
+            speed = int(line.strip())
+            if speed <0:
+                pass
+            else:
+                return speed
 
     def isInteriorLightOn(self):
         pass
