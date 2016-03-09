69e370e90b0a50912951808baacfd88847bdffc2
Magnus <magnublo@stud.ntnu.no>
2016-03-09 10:38:32 +0100

    Endret formatet på siren metoden til å ta inn argumenter i stedet for at den kaller getSpeed(), getDistance() og carIsOn()

--- VAR3_distance_to_car/main_var3.py
+++ VAR3_distance_to_car/main_var3.py
@@ -4,22 +4,9 @@ import random
 
 class distanceToCar():
 
-
-	# Returnerer distanse til bilen foran. Foreloepig redundant metode.
-	def getDistance(self):
-		return random.randrange(5, 20)
-
-	# Returnerer fart i meter per sekund. Foreloepig redundant metode.
-	def getSpeed(self):
-		return random.randrange(0,30)
-
 	# Beregner optimal distanse.
 	def calculateOptimalDistance(self,speed):
 		return speed*3
-
-	# Sjekker om bilen er paa. Foreloepig redundant metode.
-	def carIsOn(self):
-		return True
 
 	# Sirenen varsler dersom avstanden til bilen foran er for liten
 	def siren(self,carIsOn, speed, distance):
