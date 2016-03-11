3f70df4b73a76123cec817c55741e5bb937a5be7
macytter <marencol@stud.ntnu.no>
2016-03-11 10:14:27 +0100

    VAR2 nearly done

---
+++ VAR2_DefectLightbulb/lightbulbAnalyzer.py
@@ -0,0 +1,39 @@
+from carInput import *
+
+
+class lightBulbAnalyzer:
+
+#Checks if the front light sensor gets input when the car is on. Returns a message to check light if it is not.
+    def isFrontLightOff(self, frontLightSensor):
+        defectFront = carInput.isLightOff(self, frontLightSensor)
+        if defectFront == True and carInput.carIsOn(self):
+            return "Front ligth is not on. Make sure it is working"
+
+#Checks if the back light sensor gets input when the car is on. Returns a message to check light if it is not.
+    def isBackLightOff(self, backLightSensor):
+        defectBack = carInput.isLightOff(self, backLightSensor)
+        if defectBack == True and carInput.carIsOn(self):
+            return "Back ligth is not on. Make sure it is working"
+
+#Checks if the licence plate light sensor gets input when the car is on. Returns a message to check light if it is not.
+    def isLicencePlateLightOff(self, licensePlateLightSensor):
+        defectLP = carInput.isLightOff(self, licensePlateLightSensor)
+        if defectLP == True and carInput.carIsOn(self):
+            return "Licence plate ligth is not on. Make sure it is working"
+
+    def isIndicatorLightOff(self, indicatorLightSensor):
+        off = 0
+        on = 0
+        if carInput.carIsOn(self) and carInput.indicatorLightSwitchedOn(self):
+            for x in range(0,4):
+                if carInput.isLightOff(self, indicatorLightSensor) == True:
+                    off += 1
+                else:
+                    on += 1
+            if off == 5:
+                return "Please check your indicator light. No input to light sensor for 5 seconds"
+
+    def isBrakeLightOn(self, brakeLightSensor):
+        if carInput.carIsOn(self) and carInput.brakePushed(self):
+            if carInput.isLightOff(self, brakeLightSensor):
+                return "Brake light is not on. Make sure it is working"
--- carInput.py
+++ carInput.py
@@ -20,5 +20,19 @@ class carInput():
     def carIsOn(self):
         pass
 
+#Should check if switch for indicator light is activated
+    def indicatorLightSwitchedOn(self):
+        pass
+
+#Should check if brakes are used
+    def brakePushed(self):
+        pass
+
+    #The method checks if the ligthsensor recieves input. Returns False if everything is OK.
+    #sensorInput recieves either a 0, if the light is not on, and 1 if the light is on.
+    def isLightOff(self, sensorInput):
+        if sensorInput == 0:
+            return True
+        return False
 
 
