b92f0b04ba817c1c21ac89497b733f547f63d348
arnagl <arna.glavovic@gmail.com>
2016-03-14 16:59:23 +0100

    Fikset på koden, laget unit test

--- carInput.py
+++ carInput.py
@@ -14,28 +14,35 @@ class carInput():
     def isInteriorLightOn(a):
         pass
 
-    print isInteriorLightOn(1)
+    # print isInteriorLightOn(1)
 
 
     def distanceToCar(self):
         pass
 
-    def carIsOn(self):
-        pass
+    def carIsOn(self, carSignal):
+        if carSignal == 1:
+            return True
+        else:
+            return False
 
-    #Should check if switch for indicator light is activated
-    def IndicatorLightSwitchedOn(self):
-        pass
+    # Should check if switch for indicator light is activated
+    def indicatorLightSwitchedOn(self, lightSignal):
+        if lightSignal == 1:
+            return True
+        else:
+            return False
 
-    #Should check if brakes are used
-    def brakePushed(self):
-        pass
+    # Should check if brakes are used
+    def brakePushed(self, brakeSignal):
+        if brakeSignal == 1:
+            return True
+        else:
+            return False
 
-    #The method checks if the ligthsensor recieves input. Returns False if everything is OK.
-    #sensorInput recieves either a 0, if the light is not on, and 1 if the light is on.
+    # The method checks if the ligthsensor recieves input. Returns False if everything is OK.
+    # sensorInput recieves either a 0, if the light is not on, and 1 if the light is on.
     def isLightOff(self, sensorInput):
         if sensorInput == 0:
             return True
         return False
-
-
