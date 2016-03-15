db6a10d1020380b2f406a578602fedb700037c84
Magnus <magnublo@stud.ntnu.no>
2016-03-15 14:18:38 +0100

    Har skreve meir på Server

--- VAR3_distance_to_car/main_var3.py
+++ VAR3_distance_to_car/main_var3.py
@@ -5,18 +5,18 @@ import carInput
 
 class distanceToCar():
 
-	# Beregner optimal distanse.
-	def calculateOptimalDistance(self):
-		speed = carInput.getSpeed()
-		return speed*3
+    # Beregner optimal distanse.
+    def calculateOptimalDistance(self):
+        speed = carInput.getSpeed()
+        return speed*3
 
-	# Sirenen varsler dersom avstanden til bilen foran er for liten
-	def calculateIfTooCloseToCar(self):
-		speed = carInput.getSpeed()
-		distance = carInput.distanceToCarAhead()
-		carIsOn = carInput.carIsOn()
-		while carIsOn:
-			if (self.calculateOptimalDistance() > distance):
-				siren.triggeredByDistanceToCar()
-			else:
-				continue
+    # Sirenen varsler dersom avstanden til bilen foran er for liten
+    def calculateIfTooCloseToCar(self):
+        speed = carInput.getSpeed()
+        distance = carInput.distanceToCarAhead()
+        carIsOn = carInput.carIsOn()
+        while carIsOn:
+            if self.calculateOptimalDistance() > distance:
+                siren.triggeredByDistanceToCar()
+            else:
+                continue
