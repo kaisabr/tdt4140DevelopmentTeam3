fcd84016218cc3c2c205ec95f4db5f9b441a5f29
Mathilde Theisen <mathildetheisen@gmail.com>
2016-04-13 12:54:52 +0200

    Prøver å legge inn lydfil

--- siren.py
+++ siren.py
@@ -1,3 +1,5 @@
+import winsound
+
 class siren():
 
     def __init__(self):
@@ -7,11 +9,13 @@ class siren():
         print "Check if your interior light is on"
 
     def triggeredByDistanceToCarInFront(self):
+
         print "Distance to car in front is too small."
 
     def triggeredByDistanceToCar(self):
         pass
 
     def triggeredByDistanceToSide(self):
+        winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
         print "There is an object in your blind spot."
 
